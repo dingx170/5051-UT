c0792f8dcc706a5bd73b35772cd94f9a35053fb3
dingx170 <dingtong@seattleu.edu>
2019-06-07 16:57:48 -0700

    Added LogViewModel Instantiate test Case

--- UnitTests/Models/LogViewModelTests.cs
+++ UnitTests/Models/LogViewModelTests.cs
@@ -11,11 +11,16 @@ namespace UnitTests.Models
         public void LogViewModel_Instantiate_Default_Should_Pass()
         {
             // Arrange
+            // just evoking the constructor will cover 60% of the code, so no arrange is needed 
 
             // Act
+            var result = new LogViewModel();
 
             // Assert
-
+            // We don’t know what the new log model is, but we can check that it is not null.  
+            // Testing for a NOT is less desired than testing for an IS, 
+            // but we have little choice for this test so add test to validate it is not null
+            Assert.IsNotNull(result);
         }
     }
 }
