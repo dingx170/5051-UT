3cb1053b355fe4a8460a5e0c0fa18ba0e6dd7dda
dingx170 <dingtong@seattleu.edu>
2019-06-07 19:34:18 -0700

    Added the template for LogBackendTests

--- UnitTests/Backend/LogBackendTests.cs
+++ UnitTests/Backend/LogBackendTests.cs
@@ -7,8 +7,103 @@ namespace UnitTests.Backend
     public class LogBackendTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void LogBackend_Default_Instantiate_Should_Pass()
         {
+            // Arange
+
+            // Act
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void LogBackend_Index_Default_Should_Pass()
+        {
+            // Arange
+
+            // Act
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void LogBackend_Create_Valid_Item_Should_Pass()
+        {
+            // Arange
+
+            // Act
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void LogBackend_Create_InValid_Null_Should_Fail()
+        {
+            // Arange
+
+            // Act
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void LogBackend_Read_Valid_Item_Should_Pass()
+        {
+            // Arange
+
+            // Act
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void LogBackend_Read_InValid_Item_Should_Pass()
+        {
+            // Arange
+
+            // Act
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void LogBackend_Update_First_Item_Should_Pass()
+        {
+            // Arange
+
+            // Act
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void LogBackend_Update_InValid_Bogus_Item_Should_Pass()
+        {
+            // Arange
+
+            // Act
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void LogBackend_Delete_First_Item_Should_Pass()
+        {
+            // Arange
+
+            // Act
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void LogBackend_Delete_InValid_Bogus_Item_Should_Pass()
+        {
+            // Arange
+
+            // Act
+
+            // Assert
         }
     }
 }
