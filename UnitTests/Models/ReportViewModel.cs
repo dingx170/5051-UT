802dc33dba048ac18b66e12213765351b74b562e
dingx170 <dingtong@seattleu.edu>
2019-06-07 17:17:45 -0700

    Added ReportViewModel.cs to the project

---
+++ UnitTests/Models/ReportViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.Models
+{
+    [TestClass]
+    public class ReportViewModel
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
