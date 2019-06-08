d1305f7addfe2497499937b6e0628486901e0dd0
dingx170 <dingtong@seattleu.edu>
2019-06-07 19:19:15 -0700

    Added LogBackendTests.cs

---
+++ UnitTests/Backend/LogBackendTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.Backend
+{
+    [TestClass]
+    public class LogBackendTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
