556c6e11fd114fab927f40d28d1abe3505731e0b
dingx170 <dingtong@seattleu.edu>
2019-06-10 22:11:45 -0700

    Added LogBackend index test case

--- UnitTests/Backend/LogBackendTests.cs
+++ UnitTests/Backend/LogBackendTests.cs
@@ -1,29 +1,39 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HW1c.Backend;
+using System.Collections.Generic;
+using HW1c.Models;
+using System.Linq;
+using System.Web;
 
 namespace UnitTests.Backend
 {
     [TestClass]
     public class LogBackendTests
     {
+        /*
         [TestMethod]
         public void LogBackend_Default_Instantiate_Should_Pass()
         {
             // Arange
 
             // Act
+            var result = new LogBackend();
 
             // Assert
+            Assert.IsNotNull(result);
         }
-
+         */
         [TestMethod]
         public void LogBackend_Index_Default_Should_Pass()
         {
             // Arange
 
             // Act
+            var result = LogBackend.Instance.Index();
 
             // Assert
+            Assert.IsNotNull(result);
         }
 
         [TestMethod]
